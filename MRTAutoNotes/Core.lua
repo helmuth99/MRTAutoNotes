4e6ac1c0eac0c56a8233de41c39d3e235ad1fbd4
Helmuth99 <helmuth.petelin@hotmail.de>
2024-10-16 18:22:11 +0200

    refactoring

---
+++ MRTAutoNotes/Core.lua
@@ -0,0 +1,9 @@
+local addon, private = ...
+
+MRTAutoNotes = LibStub("AceAddon-3.0"):NewAddon("MRTAutoNotes", "AceConsole-3.0")
+
+
+function MRTAutoNotes:OnInitialize()
+    self:Print("welcome")
+    print("welcome")
+end
